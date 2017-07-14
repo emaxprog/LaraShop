8f2be420cdfca6f17e0269a046bf86a50e4e2420
alexandr <emaxprog@gmail.com>
2017-07-14 13:16:14 +0300

    Были добавлены новые поля к пользователю

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,44 +16,83 @@ class CreateUsersTable extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->string('name', 100)->comment('Название страны');
+            $table->string('name', 128)->comment('Название страны');
+
+            $table->index('name');
         });
 
         Schema::create('regions', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('country_id')->unsigned()->comment('Страна');
-            $table->string('name', 100)->comment('Название региона');
+            $table->string('name', 128)->comment('Название региона');
 
             $table->foreign('country_id')->references('id')->on('countries')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
+
+            $table->index('name');
         });
 
         Schema::create('cities', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('region_id')->unsigned()->comment('Регион');
-            $table->string('name', 100)->comment('Название города');
+            $table->string('name', 128)->comment('Название города');
 
             $table->foreign('region_id')->references('id')->on('regions')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
+
+            $table->index('name');
+        });
+
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('city_id')->unsigned()->comment('Город');
+            $table->string('street', 128)->comment('Улица');
+            $table->string('building', 32)->comment('Номер дома');
+            $table->string('apartment', 32)->nullable()->comment('Номер квартиры');
+            $table->string('postcode',16)->comment('Почтовый индекс');
+
+            $table->foreign('city_id')->references('id')->on('cities')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('phones', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('country_code',1)->comment('Код страны');
+            $table->string('operator_code',3)->comment('Код оператора');
+            $table->string('number',7)->comment('Номер телефона');
+
+            $table->unique(['country_code', 'operator_code', 'number']);
         });
 
         Schema::create('users', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->string('name', 30)->comment('Имя пользователя');
-            $table->string('email', 30)->unique()->comment('Email пользователя');
-            $table->string('password', 60)->comment('Пароль пользователя');
-            $table->integer('city_id')->unsigned();
+            $table->string('name', 32)->comment('Имя');
+            $table->string('surname', 64)->comment('Фамилия');
+            $table->string('email', 32)->unique()->comment('Email');
+            $table->string('password', 64)->comment('Пароль');
+            $table->integer('address_id')->unsigned()->comment('Адрес');
+            $table->integer('phone_id')->unsigned()->comment('Телефон');
             $table->rememberToken();
             $table->timestamps();
 
-            $table->foreign('city_id')->references('id')->on('cities')
+            $table->foreign('address_id')->references('id')->on('addresses')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
+
+            $table->foreign('phone_id')->references('id')->on('phones')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->index(['name', 'surname']);
+            $table->index('email');
         });
     }
 
@@ -65,6 +104,8 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('phones');
+        Schema::dropIfExists('addresses');
         Schema::dropIfExists('cities');
         Schema::dropIfExists('regions');
         Schema::dropIfExists('countries');
